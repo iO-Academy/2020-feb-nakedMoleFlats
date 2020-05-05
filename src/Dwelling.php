d9f53f5061a8ee51f9a7f268d0ebb11d26030a9d
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-05-05 09:26:28 +0100

    getters on all properties

--- src/Dwelling.php
+++ src/Dwelling.php
@@ -2,5 +2,70 @@
 
 class Dwelling 
 {
-    
+    private $agentRef;
+    private $address1;
+    private $address2;
+    private $town;
+    private $postcode;
+    private $description;
+    private $bedrooms;
+    private $price;
+    private $image;
+    private $type;
+    private $status;
+
+    public function getAgentRef()
+    {
+        return $this->agentRef;
+    }
+
+    public function getAddress1()
+    {
+        return $this->address1;
+    }
+
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getBedrooms()
+    {
+        return $this->bedrooms;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
 }

974bbf2cb7b952cf155e81bc2a559b57584e2e8b
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-05-05 15:58:16 +0100

    tidied up

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en-GB">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Naked Mole Flats</title>
-
     <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
@@ -22,133 +21,104 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat" type="text/css">
     <link rel="stylesheet" href="dist/css/styles.css" type="text/css">
     <script src="https://kit.fontawesome.com/c90803ff49.js" crossorigin="anonymous"></script>
   </head>
-  
   <body id="top">
-
     <header>
         <div class="headerContent">
             <img src="src/images/nakedMoleFlatsLogo.png" alt="Naked Mole Flats Logo">
             <div class="headerText">
                 <h1>NAKED MOLE FLATS</h1>
-                <h2>PROPERTIES OF DISTINCTION<h2>
+                <h2>PROPERTIES OF DISTINCTION</h2>
             </div>
         </div>
         <div class="headerBaseBar"></div>
     </header>
-
     <section class="allDwellingsContainer">
-
         <div class="dwellingContainer">
-            
             <div class="dwellingImageContainer">
-                    <img class="dwellingMainImage" src="src/images/testpic.jpeg">
-                    <div class="imageStatusText">SOLD</div>
-                    
+                    <img class="dwellingMainImage" src="src/images/testpic.jpeg" alt="property image">
+                    <div class="imageStatusText">SOLD</div>     
             </div>
-         
-
             <div class="dwellingInfo">
                 <div class="price">£12345</div>
                 <hr>
                 <div class="dwellingAddress info">Address, 1 Address St, Address Town</div>
                 <div class="dwellingStatus info">Property for Sale</div>
+                <!-- class name is for font awesome icon -->
                 <div class="dwellingBedrooms info"><i class="fas fa-bed"></i> 1 bedroom</div>
                 <!-- <button class="viewPropertyButton"><a>View Property</a></button> -->
             </div>
         </div>
-
         <div class="dwellingContainer">
-            
             <div class="dwellingImageContainer">
-                    <img class="dwellingMainImage" src="src/images/testpic.jpeg">
-                    <div class="imageStatusText">SOLD</div>
-                    
+                    <img class="dwellingMainImage" src="src/images/testpic.jpeg" alt="property image">
+                    <div class="imageStatusText">SOLD</div>    
             </div>
-         
-
             <div class="dwellingInfo">
                 <div class="price">£12345</div>
                 <hr>
                 <div class="dwellingAddress info">Address, 1 Address St, Address Town</div>
                 <div class="dwellingStatus info">Property for Sale</div>
+                <!-- class name is for font awesome icon -->
                 <div class="dwellingBedrooms info"><i class="fas fa-bed"></i> 1 bedroom</div>
                 <!-- <button class="viewPropertyButton"><a>View Property</a></button> -->
             </div>
         </div>
-
         <div class="dwellingContainer">
-            
             <div class="dwellingImageContainer">
-                    <img class="dwellingMainImage" src="src/images/testpic.jpeg">
-                    <div class="imageStatusText">SOLD</div>
-                    
+                    <img class="dwellingMainImage" src="src/images/testpic.jpeg" alt="property image">
+                    <div class="imageStatusText">SOLD</div> 
             </div>
-         
-
             <div class="dwellingInfo">
                 <div class="price">£12345</div>
                 <hr>
                 <div class="dwellingAddress info">Address, 1 Address St, Address Town</div>
                 <div class="dwellingStatus info">Property for Sale</div>
+                <!-- class name is for font awesome icon -->
                 <div class="dwellingBedrooms info"><i class="fas fa-bed"></i> 1 bedroom</div>
                 <!-- <button class="viewPropertyButton"><a>View Property</a></button> -->
             </div>
         </div>
-
         <div class="dwellingContainer">
-            
             <div class="dwellingImageContainer">
-                    <img class="dwellingMainImage" src="src/images/testpic.jpeg">
+                    <img class="dwellingMainImage" src="src/images/testpic.jpeg" alt="property image">
                     <div class="imageStatusText">SOLD</div>
-                    
             </div>
-         
-
             <div class="dwellingInfo">
                 <div class="price">£12345</div>
                 <hr>
                 <div class="dwellingAddress info">Address, 1 Address St, Address Town</div>
                 <div class="dwellingStatus info">Property for Sale</div>
+                <!-- class name is for font awesome icon -->
                 <div class="dwellingBedrooms info"><i class="fas fa-bed"></i> 1 bedroom</div>
                 <!-- <button class="viewPropertyButton"><a>View Property</a></button> -->
             </div>
         </div>
-
         <div class="dwellingContainer">
-            
             <div class="dwellingImageContainer">
-                    <img class="dwellingMainImage" src="src/images/testpic.jpeg">
-                    <div class="imageStatusText">SOLD</div>
-                    
+                    <img class="dwellingMainImage" src="src/images/testpic.jpeg" alt="property image">
+                    <div class="imageStatusText">SOLD</div>  
             </div>
-         
-
             <div class="dwellingInfo">
                 <div class="price">£12345</div>
                 <hr>
                 <div class="dwellingAddress info">Address, 1 Address St, Address Town</div>
                 <div class="dwellingStatus info">Property for Sale</div>
+                <!-- class name is for font awesome icon -->
                 <div class="dwellingBedrooms info"><i class="fas fa-bed"></i> 1 bedroom</div>
                 <!-- <button class="viewPropertyButton"><a>View Property</a></button> -->
             </div>
         </div>
-
-        
-
-       
     </section>
-
     <footer>
         <div class="footerText">©Copyright Naked Mole Rats 2020</div>
         <div class="toTop">
+            <!-- class name is for font awesome icon -->
             <a href="#top"><i class="fas fa-angle-double-up"></i></a>
         </div>
     </footer>
-    
   </body>
 </html>

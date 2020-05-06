da21c2ffc5567e1be6de32f76e8538316594035b
Richard Herbert <richard.herbert1980@gmail.com>
2020-05-06 09:25:26 +0100

    add daemon page to refresh db

---
+++ daemon.php
@@ -0,0 +1,14 @@
+<?php
+
+use NMF\DwellingAPI;
+use NMF\DwellingImporter;
+
+require_once 'vendor/autoload.php';
+
+$api = new DwellingAPI("https://dev.maydenacademy.co.uk/resources/property-feed/");
+
+$db = new PDO('mysql:host=db; dbname=NMF', 'root', 'password');
+
+$importer = new DwellingImporter($db, $api);
+
+$importer->refreshDB();

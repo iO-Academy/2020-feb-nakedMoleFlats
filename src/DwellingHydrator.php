d9e3ec1b1ba7caecdeef9845ab74bf9edb4ea988
“Milo <milogirardet@gmail.com>
2020-05-06 13:40:34 +0100

    initial commit

--- src/DwellingHydrator.php
+++ src/DwellingHydrator.php
@@ -32,8 +32,14 @@ class DwellingHydrator
     /**
      * takes database connection and joins data from tables to return one dwelling object
      */
-    public function loadDwelling()
-    {
-        //todo
+    public function loadSingleDwelling() {
+        $query = $this->db->prepare('SELECT `dwellings`.`dwellingId`, `dwellings`.`agentRef`, `dwellings`.`address1`, `dwellings`.`address2`, `dwellings`.`town`, `dwellings`.`postcode`, `dwellings`.`description`, `dwellings`.`bedrooms`, `dwellings`.`price`, `dwellings`.`image`, `types`.`type`, `statuses`.`status`                      
+                                    FROM ((`dwellings` 
+                                    INNER JOIN `types` ON `dwellings`.`typeId` = `types`.`typeId`) 
+                                    INNER JOIN `statuses` ON `dwellings`.`statusId` = `statuses`.`statusId`)
+                                    WHERE `dwellings`.`dwellingId` = "$_GET["id"]"'
+                                    );
+        $query->execute();
+        return $query->fetchAll(\PDO::FETCH_CLASS, 'NMF\Dwelling');
     }
 }
